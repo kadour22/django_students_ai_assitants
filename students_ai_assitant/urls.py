2a398fe7e7718688b2c9de5d11f73b9103f5ecff
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-27 22:58:59 +0100

    configuer media url

--- students_ai_assitant/urls.py
+++ students_ai_assitant/urls.py
@@ -2,7 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Documents/', include('Documents.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, docuemnt_root = settings.MEDIA_ROOT)
