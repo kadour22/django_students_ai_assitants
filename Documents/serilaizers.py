cbc28dbb448b4a2a8f329aae4f22ccd5b95d1d2b
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-02-27 23:01:46 +0100

    add validations method

--- Documents/serilaizers.py
+++ Documents/serilaizers.py
@@ -5,3 +5,12 @@ class Document_Serializer(serializers.ModelSerializer):
     class Meta :
         model = Document
         fields = ["id","title","pdf","uploaded_at"]
+    
+    def validate_pdf(self, value):
+        if value.content_type != "application/pdf":
+            raise serializers.ValidationError("Only PDF files are allowed.")
+
+        if value.size > 5 * 1024 * 1024:  # 5MB limit
+            raise serializers.ValidationError("File size must be under 5MB.")
+
+        return value
