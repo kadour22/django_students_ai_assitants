920747a3a1ae833e6f3a9774485fe2a43313d324
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-03-01 01:26:56 +0100

    fixe urls routes

--- Documents/urls.py
+++ Documents/urls.py
@@ -1,11 +1,7 @@
-# urls.py
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DocumentViewSet
 
 router = DefaultRouter()
 router.register(r'documents', DocumentViewSet, basename='documents')
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+urlpatterns = router.urls
