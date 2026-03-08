7d4d0530d9747ac5c3a1594e051035c568546a6a
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-03-08 14:21:01 +0100

    add get_document_by_id service

--- Documents/services/document_service.py
+++ Documents/services/document_service.py
@@ -30,3 +30,26 @@ class DocumentService(BaseService) :
         self.delete_cache_pattern('all_documents*')
         return document
     
+    def get_document_by_id(self, document_id: int) -> Optional[Document]:
+        cache_key = self.get_cache_key('document', id=document_id)
+
+        cached_document = self.get_from_cache(cache_key)
+        if cached_document:
+            return cached_document
+
+        try:
+            document = Document.objects.get(id=document_id)
+            self.set_cache(cache_key, document, timeout=self.document_cache_timeout)
+            return document
+        except Document.DoesNotExist:
+            return None
+    
+    # def delete_document(self, document_id: int) -> bool:
+    #     try:
+    #         document = Document.objects.get(id=document_id)
+    #         document.delete()
+    #         self.delete_cache_pattern('all_documents*')
+    #         self.delete_cache_pattern(f'document_id:{document_id}*')
+    #         return True
+    #     except Document.DoesNotExist:
+    #         return False
